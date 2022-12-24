fd2e12003b5fa6c19f741544e3087e7e42c2e019
itsjahidhasan <mdjahidhasanf92@gmail.com>
2022-12-25 03:15:35 +0600

    API complete

---
+++ TrackingAPI/Models/Issue.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackingAPI.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priorty Priorty { get; set; }
+        public IssueType IssueType { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+    }
+    public  enum Priorty
+    {
+        low,
+        Meduim,
+        High,  
+    }
+    public enum IssueType
+    {
+        Feature,Bug,Documentation
+    }
+}
